f9384a72adf8861b2518e895ffbc495c4fecec6f
Brian Birmingham <4brianbirmingham@gmail.com>
2019-07-19 06:08:52 -0500

    another

---
+++ Python_101/logging/combined.py
@@ -0,0 +1,18 @@
+import logging
+
+# add filemode="w" to overwrite
+logging.basicConfig(filename="sample.log", filemode="w", level=logging.WARNING)
+
+logging.debug("This is a debug message")
+logging.info("Informational message")
+logging.warning("This is a warning message")
+logging.error("An error has happened!")
+logging.critical("This is critical.  Very bad")
+
+logging.basicConfig(filename="sample.log", level=logging.INFO)
+log = logging.getLogger("ex")
+
+try:
+    raise RuntimeError
+except RuntimeError:
+    log.exception("Error!")
