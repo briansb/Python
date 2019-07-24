db2a04858be63998537518a063d07c9770747990
Brian Birmingham <4brianbirmingham@gmail.com>
2019-07-24 06:16:52 -0500

    Wednesday

--- Python_101/os_module/AllOfThem.py
+++ Python_101/os_module/AllOfThem.py
@@ -2,5 +2,19 @@ import os
 
 print("Platform is {0}".format(os.name))
 
+# will return an error if DESTOP_SESSION not present
 print(f"Desktop session is {os.environ['DESKTOP_SESSION']}")
+
+# will return None if not present
+print(f"Path is {os.getenv('PATH2')}")
+
+# change directory, get current working directory
+os.chdir("/home/user/Linux")
+print(os.getcwd())
+
+# make a directory
+os.mkdir("/home/user/TempDir")
+# make a path of directories
+path = "/home/user/Temp2Dir/Brian/Birmingham/Beth/Brooks"
+os.makedirs(path)
 
