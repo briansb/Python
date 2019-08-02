c8130576933088489d6d381a49544b798d9317a1
Brian Birmingham <4brianbirmingham@gmail.com>
2019-08-02 06:05:58 -0500

    Friday

--- Python_101/email&smtplib/send_email_v1.py
+++ Python_101/email&smtplib/send_email_v1.py
@@ -3,31 +3,33 @@
 
 import smtplib
 
-gmail_user = '4brianbirmingham'
-gmail_password = 'Luger9mm'
+def send_email(host, subject, to_address, from_address, body_text):
+    BODY = "\r\n".join((
+        "From: %s" % from_address,
+        "To: %s" % to_address,
+        "Subject: %s" % subject,
+        "",
+        body_text))
 
-sent_from = gmail_user
-to = ['brian.birmingham@odysseysr.com', '4brianbirmingham@gmail.com']
-subject = 'OMG Super Important Message'
-body = 'Hey, whats up?\n\n- This is a test message'
+    try:
+        server = smtplib.SMTP_SSL(host, 465)
+        server.ehlo()
+        gmail_user = '4brianbirmingham'
+        gmail_password = 'Luger9mm'
+        server.login(gmail_user, gmail_password)
+        server.sendmail(from_address, [to_address], BODY)
+        server.close()
+        print('Email sent!')
+    except Exception as e:
+        print("Didn't work\n")
+        print(e.__doc__)
+        print(e.message)
 
-email_text = """\
-From: %s
-To: %s
-Subject: %s
 
-%s
-""" % (sent_from, ", ".join(to), subject, body)
-
-try:
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    server.login(gmail_user, gmail_password)
-    server.sendmail(sent_from, to, email_text)
-    server.close()
-
-    print('Email sent!')
-except Exception as e:
-    print("Didn't work\n")
-    print(e.__doc__)
-    print(e.message)
+if __name__ == "__main__":
+    host = "smtp.gmail.com"
+    subject = "OMG. You MUST read this."
+    to_address = "4brianbirmingham@gmail.com"
+    from_address = "4brianbirmingham@gmail.com"
+    body_text = "Hey.  Hey.  Hey."
+    send_email(host, subject, to_address, from_address, body_text)
