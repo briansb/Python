1ec9fa15d6b8ec2aafb7059097cf2b6404622df3
Brian Birmingham <4brianbirmingham@gmail.com>
2019-08-13 06:14:44 -0500

    Tuesday

--- Python_101/sys/sysargv.py
+++ Python_101/sys/sysargv.py
@@ -1,7 +1,13 @@
 import sys
+import subprocess
 
 # run from the command line, sys.argv returns the parameters
 print(sys.argv)
 
 # path to Python executable
 print(sys.executable)
+
+# call a program that calls exit
+code = subprocess.call(["python", "exit.py"])
+print(f"Exit code = {code}")
+
