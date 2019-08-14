ef837d4feaf33514a4a972fdaab88244ce02cf46
Brian Birmingham <4brianbirmingham@gmail.com>
2019-08-14 06:16:53 -0500

    Wednesday

--- Python_101/sys/sysargv.py
+++ Python_101/sys/sysargv.py
@@ -11,3 +11,20 @@ print(sys.executable)
 code = subprocess.call(["python", "exit.py"])
 print(f"Exit code = {code}")
 
+# the search path for modules.  This is NOT the system PATH variable
+#  this is PYTHONPATH
+print(f"The system path is \n{sys.path}")
+sys.path.append("/home/usr/anotherpath")
+print(f"The appended system path is \n{sys.path}")
+sys.path.remove("/home/usr/anotherpath")
+print(f"The system path is \n{sys.path}")
+
+# the platform
+print(f"The OS platform is {sys.platform}")
+
+# can redirect stdin, stdout, stderr
+original = sys.stdout
+sys.stdout = open('temporary_stdout.txt', 'w')
+print('This is your redirected text:')
+sys.stdout = original
+print('Back to normal')
