c10d21ff6ba0567efee533f5b5077fe804022b9c
Brian Birmingham <4brianbirmingham@gmail.com>
2019-09-14 22:08:17 -0500

    Saturday

--- Python_101/decorators/decorator_v1.py
+++ Python_101/decorators/decorator_v1.py
@@ -1,21 +1,19 @@
 def another_function(func):
-    """
-    A function that accepts another function
-    """
-    
-    def other_func():
-        val = "The result of %s is %s" % (func(),
-                                          eval(func())
-                                          )
-        return val
-    return other_func
 
+##    def other_func():
+##        val = "The result of %s is %s" % (func(), eval(func()))
+##        return val
+##    return other_func
+    
+#      this doesn't work because you have to return a function
+#      the commented lines above return a function
+       val = "The result of %s is %s" % (func(), eval(func()))
+       return val
+
+@another_function
 def a_function():
-    """A pretty useless function"""
     return "1+1"
 
 if __name__ == "__main__":
     value = a_function()
     print(value)
-    decorator = another_function(a_function)
-    print(decorator())    
