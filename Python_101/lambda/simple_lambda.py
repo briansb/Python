99c4940e66f76c56827b0782dcc8e4e5da02474c
Brian Birmingham <4brianbirmingham@gmail.com>
2019-09-19 06:06:16 -0500

    Thursday

---
+++ Python_101/lambda/simple_lambda.py
@@ -0,0 +1,11 @@
+import math
+
+# standard square root
+def sqroot(x):
+    return math.sqrt(x)
+
+print( sqroot(49) )  
+
+# lambda function
+square_rt = lambda x: math.sqrt(x)
+print (square_rt(49))
