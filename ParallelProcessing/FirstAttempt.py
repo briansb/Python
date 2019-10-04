53b116415df5bcff3f5b79c7583b91961ae05e14
Brian Birmingham <brian.birmingham@odysseysr.com>
2019-10-04 16:46:05 -0500

    Friday

--- ParallelProcessing/FirstAttempt.py
+++ ParallelProcessing/FirstAttempt.py
@@ -17,7 +17,7 @@ print(f"Number of processors: {mp.cpu_count()}")
 # Problem - how many numbers are present, in a given range, in each row?
 # Prepare data - a 2D matrix (or list of lists)
 # change number of rows to 200000
-numberofrows = 1000000
+numberofrows = 5000000
 
 # sets the random seed without impacting the global numpy state...whatever that means
 np.random.RandomState(100)
@@ -25,9 +25,15 @@ np.random.RandomState(100)
 arr = np.random.randint(0, 10, size=[numberofrows, 5])
 # converts the array into a list
 data = arr.tolist()
-print(f"Data = {data[:5]}")
+# these two statements need to be swapped for large numberofrows
+#print(f"Data = {data}")
+print(f"Data = {data[:10]}")
 print()
 
+lower = 4
+upper = 8
+print(f"In the above array of lists, the numbers are random, 0 -> 9")
+print(f"Problem: For each row (list), how many numbers lie between {lower} and {upper}?")
 
 print("Beginning slow solution")
 start = time.time()
@@ -42,7 +48,9 @@ def howmany_within_range(row, minimum, maximum):
 
 results = []
 for row in data:
-    results.append(howmany_within_range(row, minimum=4, maximum=8))
+    results.append(howmany_within_range(row, minimum=lower, maximum=upper))
 end = time.time()
+# swap for large numberofrows
+#print(f"Solution = {results}")
 print(f"Solution = {results[:10]}")
 print(f"Elapsed time = {(end - start):.6f}")
