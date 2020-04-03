cd11c0df3bb21a02f36fa79b39c732692144930f
briansb <4brianbirmingham@gmail.com>
2020-04-03 16:18:19 -0500

    Friday

--- Plotting/Matplotlib/Learning_v01.py
+++
@@ -1,9 +0,0 @@
-#   https://realpython.com/python-matplotlib-guide/
-#  Python Plotting with matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-
-np.random.seed(444)
-fig = plt.figure()
-fig.suptitle('No axes on this figure')
-fig, ax_lst = plt.subplots(2,2)
--- Plotting/Matplotlib/Learning_v1.py
+++
@@ -1,6 +0,0 @@
-#   https://realpython.com/python-matplotlib-guide/
-#  Python Plotting with matplotlib
-import matplotlib.pyplot as plot
-import numpy as np
-
-np.random.seed(444)
